cecee3dc95a82bce937f196663ca660d6233b60c
larisasirokih <Lorashirokikh@yandex.ru>
2024-06-02 23:03:11 +0300

    brand cloud add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import BrandCloud from "@/components/BrandCloud";
 import CatalogList from "@/components/CatalogList";
 import CategoryList from "@/components/CategoryList";
 import ProductList from "@/components/ProductList";
@@ -6,13 +7,33 @@ import Slider from "@/components/Slider";
 import WiteList from "@/components/WiteList";
 import { Suspense } from "react";
 
+
+const brands = [
+  { name: "Labirint", logo: "/labirint.png" },
+  { name: "intecron", logo: "/intecron.png" },
+  { name: "Asd", logo: "/Asd.png" },
+  { name: "zd.png", logo: "/zd.png" },
+  { name: "Doormag", logo: "/doormag.webp" },
+  { name: "argus.svg", logo: "/argus.svg" },
+  // Добавьте больше брендов по необходимости
+];
+
 const HomePage = async () => {
   const categoryId = '665b2da0845f4980629d771d';
   
   return (
     <div>
       <Slider />
-      <div className="mt-24">
+      <div className="mt-15">
+        {/* <h1 className="text-2xl px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mb-12">
+          Наши Бренды
+        </h1> */}
+        <Suspense fallback={<Skeleton />}>
+          <BrandCloud brands={brands} />
+        </Suspense>
+      </div>
+      
+      <div className="mt-20">
         <h1 className="text-2xl px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mb-12">
           Белые двери
         </h1>
---
+++ src/components/BrandCloud.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import Image from "next/image";
+
+const BrandCloud = ({
+  brands,
+}: {
+  brands: { name: string; logo: string }[];
+}) => {
+  return (
+    <div className="py-8 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+      
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 justify-items-center">
+        {brands.map((brand) => (
+          <div
+            key={brand.name}
+            className="relative w-24 h-24 md:w-32 md:h-32 lg:w-40 lg:h-40 p-2 hover:scale-105 transition-transform duration-300"
+          >
+            <Image
+              src={brand.logo}
+              alt={brand.name}
+              fill
+              sizes="20vw"
+              className="object-contain"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BrandCloud;
