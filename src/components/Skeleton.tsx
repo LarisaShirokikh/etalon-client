1e71f4ed136c2e55fdf796d7a3ee5c1b04ad4f90
larisasirokih <Lorashirokikh@yandex.ru>
2024-06-07 00:14:09 +0300

    skeleton

--- src/components/Skeleton.tsx
+++ src/components/Skeleton.tsx
@@ -1,49 +1,24 @@
 const Skeleton = () => {
   return (
-    <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap animate-pulse ">
-      <div className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
-        <div className="w-full h-80 bg-gray-100 rounded-md" />
-        <div className="w-full flex justify-between ">
-          <div className="w-36 h-8 bg-gray-100 rounded-md" />
-          <div className="w-16 h-8 bg-gray-100 rounded-md  ml-auto" />
-          <div className="" />
-        </div>
-        <div className="w-full h-4 bg-gray-100 rounded-md " />
-        <div className="w-1/2 h-4 bg-gray-100 rounded-md " />
-        <div className="w-1/2 h-12 bg-gray-100 rounded-2xl " />
-      </div>
-      <div className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
-        <div className="w-full h-80 bg-gray-100 rounded-md" />
-        <div className="w-full flex justify-between ">
-          <div className="w-36 h-8 bg-gray-100 rounded-md" />
-          <div className="w-16 h-8 bg-gray-100 rounded-md  ml-auto" />
-          <div className="" />
-        </div>
-        <div className="w-full h-4 bg-gray-100 rounded-md " />
-        <div className="w-1/2 h-4 bg-gray-100 rounded-md " />
-        <div className="w-1/2 h-12 bg-gray-100 rounded-2xl " />
-      </div>
-      <div className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
-        <div className="w-full h-80 bg-gray-100 rounded-md" />
-        <div className="w-full flex justify-between ">
-          <div className="w-36 h-8 bg-gray-100 rounded-md" />
-          <div className="w-16 h-8 bg-gray-100 rounded-md  ml-auto" />
-          <div className="" />
-        </div>
-        <div className="w-full h-4 bg-gray-100 rounded-md " />
-        <div className="w-1/2 h-4 bg-gray-100 rounded-md " />
-        <div className="w-1/2 h-12 bg-gray-100 rounded-2xl " />
-      </div>
-      <div className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
-        <div className="w-full h-80 bg-gray-100 rounded-md" />
-        <div className="w-full flex justify-between ">
-          <div className="w-36 h-8 bg-gray-100 rounded-md" />
-          <div className="w-16 h-8 bg-gray-100 rounded-md  ml-auto" />
-          <div className="" />
-        </div>
-        <div className="w-full h-4 bg-gray-100 rounded-md " />
-        <div className="w-1/2 h-4 bg-gray-100 rounded-md " />
-        <div className="w-1/2 h-12 bg-gray-100 rounded-2xl " />
+    <div className="mt-12 px-1 sm:px-5">
+      <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {[...Array(8)].map((_, index) => (
+          <div
+            key={index}
+            className="flex flex-col gap-2 group p-2 bg-white rounded-md"
+          >
+            <div className="relative w-full h-48 overflow-hidden rounded-md">
+              <div className="w-full h-full bg-gray-100 animate-pulse" />
+            </div>
+            <div className="flex flex-col gap-2 flex-grow">
+              <div className="w-full h-4 bg-gray-100 animate-pulse rounded-md" />
+              <div className="w-full h-4 bg-gray-100 animate-pulse rounded-md" />
+              <div className="w-1/2 h-12 bg-gray-100 animate-pulse rounded-2xl" />
+            </div>
+            <div className="text-sm text-gray-500 w-full h-16 bg-gray-100 animate-pulse rounded-md" />
+            {/* <Button text="Вызвать замерщика" /> */}
+          </div>
+        ))}
       </div>
     </div>
   );
