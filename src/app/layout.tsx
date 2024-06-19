ef14a9216a04edca1d9a6b3620a2c8ce70810ce6
larisasirokih <Lorashirokikh@yandex.ru>
2024-06-19 22:18:47 +0300

    yandex metrika

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,8 @@ import "@/utils/wdyr";
 import MobileNavbar from "@/components/Menu/MobileNavbar";
 import { authOptions } from "@/lib/authOptions";
 import { getServerSession } from "next-auth";
+import Script from "next/script";
+import { Suspense } from "react";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -33,9 +35,27 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Script id="metrika-counter" strategy="afterInteractive">
+          {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+ 
+              ym(97508850, "init", {
+                    defer: true,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    webvisor:true
+              });`}
+        </Script>
+        <Suspense fallback={<></>}>
+          
         <YandexMetrika />
+        </Suspense>
         <TranslationsProvider translations={translations}>
-          <Navbar  />
+          <Navbar />
           {children}
           <MobileNavbar session={session} />
           <Footer />
