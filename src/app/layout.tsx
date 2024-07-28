4494254d0fe2c52e5db0cbf555ef79181907c059
larisasirokih <Lorashirokikh@yandex.ru>
2024-07-28 16:49:50 +0300

    metrika

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,7 +53,7 @@ export default function RootLayout({
               k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
               (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
  
-              ym(97508850, "init", {
+              ym(97935366, "init", {
                     defer: true,
                     clickmap:true,
                     trackLinks:true,
