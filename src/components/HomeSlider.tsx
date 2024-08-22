b42328c31d72692d9f1d4061dae3555085591ee5
larisasirokih <Lorashirokikh@yandex.ru>
2024-08-22 10:58:20 +0300

    fix banner format

--- src/components/HomeSlider.tsx
+++ src/components/HomeSlider.tsx
@@ -1,50 +1,29 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Image from "next/image";
 
-const SimpleSlider = () => {
-  const [isMobile, setIsMobile] = useState(false);
-
-  const mobileSlide = {
-    id: 1,
-    imageUrl: "/slice.png",
-    altText: "Мобильный баннер",
-    width: 600,
-    height: 300,
+const ResponsiveBanner = () => {
+  const banner = {
+    imageUrl: "/slice.webp", // Универсальное изображение
+    altText: "Адаптивный баннер",
+    blurDataURL: "data:image/jpeg;base64,...",
   };
-
-  const desktopSlide = {
-    id: 2,
-    imageUrl: "/slice1.png",
-    altText: "Десктоп баннер",
-    width: 1200,
-    height: 600,
-  };
-
-  useEffect(() => {
-    const handleResize = () => setIsMobile(window.innerWidth <= 768);
-
-    // Проверяем размер экрана при первом рендере
-    handleResize();
-
-    window.addEventListener("resize", handleResize);
-
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  const slide = isMobile ? mobileSlide : desktopSlide;
 
   return (
     <div className="p-6 overflow-hidden rounded-lg">
       <Image
-        src={slide.imageUrl}
-        alt={slide.altText}
-        width={slide.width}
-        height={slide.height}
+        src={banner.imageUrl}
+        alt={banner.altText}
+        layout="responsive" // Автоматически подстраивает изображение под размеры контейнера
+        width={1200} // Базовая ширина изображения для десктопа
+        height={600} // Базовая высота изображения для десктопа
         className="w-full rounded-lg"
-        priority // Ускорение загрузки изображения
+        priority // Высокий приоритет для загрузки баннера
+        sizes="(max-width: 768px) 100vw, 50vw" // Установка размеров для адаптивной загрузки
+        placeholder="blur"
+        blurDataURL={banner.blurDataURL} // Размытие до полной загрузки изображения
       />
     </div>
   );
 };
 
-export default SimpleSlider;
+export default ResponsiveBanner;
